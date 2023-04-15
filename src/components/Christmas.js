b8c1c0020fd1c0f6cf899ea26df632bf0d34ad01
kechillosh <kechillosh@gmail.com>
2023-04-15 17:49:24 +0300

    almost done

--- src/components/Christmas.js
+++ src/components/Christmas.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../styles/NewYears.css";
+import "../styles/Christmas.css";
 
 function Christmas() {
-  return <div>Christmas</div>;
+  const [countdown, setCountdown] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const target = new Date(now.getFullYear(), 11, 25); // Christmas Day of current year
+      if (target < now) {
+        target.setFullYear(now.getFullYear() + 1); // Christmas Day of next year
+      }
+      const remainingTime = target - now;
+
+      setCountdown({
+        days: Math.floor(remainingTime / (1000 * 60 * 60 * 24)),
+        hours: Math.floor(
+          (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ),
+        minutes: Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds: Math.floor((remainingTime % (1000 * 60)) / 1000),
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="main-container2">
+      <h1>Christmas</h1>
+      <div className="countdown-container">
+        <div className="days">
+          <p>{countdown.days}</p>
+          <span>days</span>
+        </div>
+        <div className="hours">
+          <p>{countdown.hours}</p>
+          <span>hours</span>
+        </div>
+        <div className="mins">
+          <p>{countdown.minutes}</p>
+          <span>mins</span>
+        </div>
+        <div className="seconds">
+          <p>{countdown.seconds}</p>
+          <span>seconds</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Christmas;
--- src/components/Summer.js
+++ src/components/Summer.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../styles/NewYears.css";
+import "../styles/Summer.css";
 
 function Summer() {
-  return <div>Summer</div>;
+  const [countdown, setCountdown] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const target = new Date(now.getFullYear(), 5, 21); // June 21st of current year
+      if (target < now) {
+        target.setFullYear(now.getFullYear() + 1); // June 21st of next year
+      }
+      const remainingTime = target - now;
+
+      setCountdown({
+        days: Math.floor(remainingTime / (1000 * 60 * 60 * 24)),
+        hours: Math.floor(
+          (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        ),
+        minutes: Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds: Math.floor((remainingTime % (1000 * 60)) / 1000),
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="main-container3">
+      <h1>Summer Countdown</h1>
+      <div className="countdown-container">
+        <div className="days">
+          <p>{countdown.days}</p>
+          <span>days</span>
+        </div>
+        <div className="hours">
+          <p>{countdown.hours}</p>
+          <span>hours</span>
+        </div>
+        <div className="mins">
+          <p>{countdown.minutes}</p>
+          <span>mins</span>
+        </div>
+        <div className="seconds">
+          <p>{countdown.seconds}</p>
+          <span>seconds</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Summer;
